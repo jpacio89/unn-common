55877516545cec40ed43f1a45a26bbce3cc4a293
Joao Coelho <joao.coelho@bontouch.com>
2021-01-13 21:00:50 +0000

    no message

--- src/main/java/com/unn/common/transformers/TransformerDescriptor.java
+++ src/main/java/com/unn/common/transformers/TransformerDescriptor.java
@@ -1,6 +1,7 @@
 package com.unn.common.transformers;
 
 public class TransformerDescriptor {
+    private String name;
     private String code;
 
     public TransformerDescriptor() { }
@@ -14,9 +15,20 @@ public class TransformerDescriptor {
     }
 
     public TransformerDescriptor withCode(String code) {
-        this.code = code;
+        this.setCode(code);
         return this;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TransformerDescriptor withName(String name) {
+        this.setName(name);
+        return this;
+    }
 }
