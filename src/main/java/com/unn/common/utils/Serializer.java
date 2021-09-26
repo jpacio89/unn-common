0036da7e07554a5d93362b21026c5ec1f59ad2f6
Joao Coelho <joao.coelho@bontouch.com>
2021-09-26 01:33:13 +0100

    no message

--- src/main/java/com/unn/common/utils/Serializer.java
+++ src/main/java/com/unn/common/utils/Serializer.java
@@ -1,8 +1,6 @@
 package com.unn.common.utils;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class Serializer {
     private static final String version = "v1";
@@ -20,4 +18,26 @@ public class Serializer {
             i.printStackTrace();
         }
     }
+
+    public static Object read(String path, String extension) {
+        String fullPath = String.format("%s.%s.%s", path, version, extension);
+        Object o = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(fullPath);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            o = in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return o;
+        } catch (ClassNotFoundException c) {
+            System.out.printf("Deserializing not possible for object %s", fullPath);
+            c.printStackTrace();
+            return o;
+        }
+        return o;
+    }
+
+
 }
